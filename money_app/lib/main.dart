c70d4284c96719c9f718c30776838baf044f5c09
eldarheleg <eldar.heleg@gmail.com>
2022-10-25 08:07:58 +0200

    added firebase to project

--- money_app/lib/main.dart
+++ money_app/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'modules/home/views/transactions_screen.dart';
 import 'modules/pay_amount/views/pay_input_amount.dart';
 import 'modules/pay_name/views/pay_input_name.dart';
 import 'modules/loan_application/views/loan_input_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MoneyApp());
 }
 
@@ -35,10 +38,10 @@ class MoneyApp extends StatelessWidget {
         ),
         home: TransactionsScreen(),
         getPages: [
-          GetPage(name: "/", page:() =>  TransactionsScreen()),
-          GetPage(name: "/PayInputAmount", page:() =>  PayInputAmount()),
-          GetPage(name: "/PayInputName", page:() =>  PayInputName()),
-          GetPage(name: "/LoanInput", page:() =>  LoanInputScreen()),
+          GetPage(name: "/", page: () => TransactionsScreen()),
+          GetPage(name: "/PayInputAmount", page: () => PayInputAmount()),
+          GetPage(name: "/PayInputName", page: () => PayInputName()),
+          GetPage(name: "/LoanInput", page: () => LoanInputScreen()),
         ],
       ),
       designSize: const Size(375, 812),
--- money_app/lib/models/Transaction.dart
+++ money_app/lib/models/Transaction.dart
@@ -15,7 +15,26 @@ class Transaction {
       required this.createdAt});
 
   static List<Transaction> transactions = [
-    Transaction(id: 1, type:Types.payment, name: 'narudzba', amount: 23.00, createdAt: DateTime.now()),
-    Transaction(id: 2, type:Types.top_up, name: 'narudzba2', amount: 55.00, createdAt: DateTime.now()),
+    Transaction(
+        id: 1,
+        type: Types.payment,
+        name: 'narudzba',
+        amount: 23.00,
+        createdAt: DateTime.now()),
+    Transaction(
+        id: 2,
+        type: Types.top_up,
+        name: 'narudzba2',
+        amount: 55.00,
+        createdAt: DateTime.now()),
   ];
+
+  static Transaction formJson(Map<String, dynamic> json) {
+    return Transaction(
+        id: json['id'],
+        type: json['type'],
+        name: json['name'],
+        amount: json['amount'],
+        createdAt: DateTime.now());
+  }
 }
